1469fa7d38b77d55d15db5be6876b10c37094024
tzw0 <e0310410@u.nus.edu>
2021-10-31 06:50:24 +0800

    Updates

---
+++ static/js/main.36bf7ac9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprecontre=this.webpackJsonprecontre||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(9),t(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"asdasd"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.36bf7ac9.chunk.js.map
